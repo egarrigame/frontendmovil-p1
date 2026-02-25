f30ee01f10f2d06c3e9bdd98ca1290bf373eade6
egarrigame <egarrigame@uoc.edu>
2026-02-25 17:38:11 +0100

    final version: overall ux & detail btn-close

--- src/app/components/detail/detail.ts
+++ src/app/components/detail/detail.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'; // Añdimos input para que se pueda usar
+import { Component, Input, Output, EventEmitter } from '@angular/core'; // Añdimos input, output y eventEmitter para que se puedan usar
 import { CommonModule } from '@angular/common'; // Necesario para *ngFor *ngIf
 
  
@@ -13,4 +13,10 @@ import { Media } from '../media/media'; // Componente media para incluir el repr
 })
 export class Detail {
   @Input() player?: Player; // Aquí le decimos a Angular que la información (la variable) vendrá de otro componente (un player de players). El html lee la variable y puede recibir datos desde players.
+
+  @Output() cerrarDetail = new EventEmitter<void>(); // Emite un evento vacío para que /players.html ponga en undefined el jugador seleccionado y así se cierra el detalle
+
+  botonX() { // función que llamaremos desde /detail.html con el botón para cerrar el detalle
+    this.cerrarDetail.emit();  // se emite el evento del output
+  }
 }
